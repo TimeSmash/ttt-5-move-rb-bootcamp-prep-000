84e182b0469ddc9f0d414535e10896b505e3912d
mcumm64 <mcumm64@gmail.com>
2019-03-15 21:19:59 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,8 +12,8 @@ cells = ["","","","","","","","",""]
 
 
 def input_to_index (num) #Want cell 1 means fill board[0]
-  converted_input = (num).to_i
-  puts converted_input - 1
+  converted_input = (num - 1).to_i
+  puts converted_input
 end
   
 def update_array_at_with (array, index, value = "X")
