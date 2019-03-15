839907ef24ac08c84718342a631d156933fd8422
mcumm64 <mcumm64@gmail.com>
2019-03-15 20:29:12 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,16 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+cells = ["","","","","","","","",""]
+
 # code your input_to_index and move method here!
+
+cell = gets.strip #MOVE THIS TO BIN
+
+def input_to_index (num) #Want cell 1 means fill board[0]
+  puts  (num - 1).to_i
+end
+  
+def update_array_at_with (board_array, index, character = "X")
+  board_array[index] = character
+end
